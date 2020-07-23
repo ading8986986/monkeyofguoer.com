1997fd63c858da9c16ea03c5393c9915838511da
dingting.dt <dingting.dt@alibaba-inc.com>
2020-07-22 21:26:20 -0400

    第一版

--- vue-now-ui-kit-master/src/pages/components/CarouselSection.vue
+++ vue-now-ui-kit-master/src/pages/components/CarouselSection.vue
@@ -1,34 +1,35 @@
 <template>
-  <div class="section" id="carousel">
-    <div class="container">
-      <div class="title">
-        <h4>Carousel</h4>
-      </div>
+  <div id="carousel">
       <div class="row justify-content-center">
-        <div class="col-8">
-          <el-carousel height="500px">
+        <div class="col-12">
+          <el-carousel height="800px">
             <el-carousel-item>
-              <img class="d-block" src="img/bg1.jpg" alt="First slide" />
+              <img class="d-block" src="img/bg1.png" alt="First slide" />
               <div class="carousel-caption d-none d-md-block">
-                <h5>Nature, United States</h5>
+                <h5>2016.07.23</h5>
               </div>
             </el-carousel-item>
             <el-carousel-item>
-              <img class="d-block" src="img/bg3.jpg" alt="Second slide" />
+              <img class="d-block" src="img/bg2.png" alt="rond slide" />
               <div class="carousel-caption d-none d-md-block">
-                <h5>Somewhere Beyond, United States</h5>
+                <h5>2017.07.23</h5>
               </div>
             </el-carousel-item>
             <el-carousel-item>
-              <img class="d-block" src="img/bg4.jpg" alt="Third slide" />
+              <img class="d-block" src="img/bg3.png" alt="Third slide" />
               <div class="carousel-caption d-none d-md-block">
-                <h5>Yellowstone National Park, United States</h5>
+                <h5>2018.07.23&2019.07.23</h5>
+              </div>
+            </el-carousel-item>
+            <el-carousel-item>
+              <img class="d-block" src="img/bg4.png" alt="Fourth slide" />
+              <div class="carousel-caption d-none d-md-block">
+                <h5>2020.07.23</h5>
               </div>
             </el-carousel-item>
           </el-carousel>
         </div>
       </div>
-    </div>
   </div>
 </template>
 <script>
